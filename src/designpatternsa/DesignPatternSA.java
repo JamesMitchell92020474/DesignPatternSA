f98b572f5663628f9af4d5d7dac0841ddc97e281
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-26 12:59:13 +1200

    DesignPatternSA Final Commit

--- src/designpatternsa/DesignPatternSA.java
+++ src/designpatternsa/DesignPatternSA.java
@@ -17,7 +17,24 @@ public class DesignPatternSA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Starting the simple bingo game...");
+        
+        //Creating obervable and observer objects
+        NumberCalls observable = new NumberCalls();
+        Player player1 = new Player("Bob");
+        Player player2 = new Player("Sue");
+        Player player3 = new Player("Tim");
+        
+        //subscribing observers to the observable object that will call out the numbers
+        observable.registerObserver(player1);
+        observable.registerObserver(player2);
+        observable.registerObserver(player3);
+        //the object will call out the numbers until the vicrtory condition is achieved by one of the subscribed objects
+        while(observable.checkVictoryCondition() == false){
+            observable.generateRandomNumber();        
+        }
+        //the program terminates when a winner is announced
+        System.exit(0);
     }
     
 }
