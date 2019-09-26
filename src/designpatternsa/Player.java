aa46e685a19ae769bc519dd916b06a9d610cc0c9
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-26 12:47:10 +1200

    Player Initial Commit

---
+++ src/designpatternsa/Player.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatternsa;
+
+/**
+ *
+ * @title Player.java
+ * @author James Mitchell 92020474
+ * @date 26/09/2019
+ */
+public class Player {
+    
+}
